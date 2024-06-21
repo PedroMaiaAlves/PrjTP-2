d4ac23c7f604f9bdfdf65cd2ecba726afb3a50ff
Pedro Henrique Maia Alves <160970721+PedroMaiaAlves@users.noreply.github.com>
2024-06-21 19:56:56 -0300

    Add files via upload

---
+++ PrjTP-2/JS/profile.js
@@ -0,0 +1,56 @@
+const perfil = document.querySelector('.profile');
+
+function getApiGitHubProfile() {
+    fetch('https://api.github.com/users/PedroMaiaAlves')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+            
+            
+            let profilePhotoDiv = document.createElement('div');
+            profilePhotoDiv.classList.add('perfilEfoto');
+
+            profilePhotoDiv.innerHTML = `
+                <div class="centroResponsivo">
+                    <img class="imgPerfil" src="${data.avatar_url}" alt="Foto de Perfil">
+                </div>
+            `;
+
+            perfil.appendChild(profilePhotoDiv);
+
+            
+            let profile = document.createElement('div');
+            profile.classList.add('nomeEtexto');
+
+            profile.innerHTML = `
+                <a class="fs-1" href="https://github.com/PedroMaiaAlves">${data.name}</a>
+                <p class="textoPerfil">${data.bio}</p>
+                <p>Localização: ${data.location} </p>
+                <a href="${data.blog}"> Site: ${data.blog}</a>
+            `;
+
+            perfil.appendChild(profile);
+
+            
+            let socialDiv = document.createElement('div');
+            socialDiv.classList.add('redesSociais');
+
+            socialDiv.innerHTML = `
+                <p>Redes Sociais</p>
+                <a href="${data.html_url}">GitHub</a>
+                <a href="https://www.instagram.com/sloowty/">Instagram</a>
+                <a href="https://www.linkedin.com/in/pedro-henrique-maia-alves-978397291/">Linkedin</a>
+                <p>Seguidores no Git: ${data.followers}</p>
+            `;
+
+            perfil.appendChild(socialDiv);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar os dados do GitHub:', error);
+        });
+}
+
+getApiGitHubProfile();
---
+++ PrjTP-2/JS/repos.js
@@ -0,0 +1,37 @@
+const repositorio = document.querySelector('.allCards');
+
+function getApiGitHubRepos() {
+    fetch('https://api.github.com/users/PedroMaiaAlves/repos')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            let data = await res.json();
+
+            data.forEach((repo, index) => {
+                let repos = document.createElement('div');
+                repos.classList.add('card-body');
+
+                repos.innerHTML = `
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${repo.name}</h5>
+                            <p class="card-text">${repo.description}</p>
+                            <a href="repo${index + 1}.html" class="card-link">Saiba Mais</a>
+                            <a href="${repo.html_url}" class="card-link">Créditos</a>
+                            <p>Forks: ${repo.forks}</p>
+                            <p>Stars: ${repo.stargazers_count}</p>
+                        </div>
+                    </div>
+                `;
+
+                repositorio.appendChild(repos);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar os dados do GitHub:', error);
+        });
+}
+
+getApiGitHubRepos();
